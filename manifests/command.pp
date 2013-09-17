5be8d5f6f03423c4158f497a9196260c9693aac2
Jeremy Kitchen <kitchen@kitchen.io>
2013-09-17 00:25:46 +0000

    allow for shorter commands by prepending the path

--- manifests/command.pp
+++ manifests/command.pp
@@ -6,6 +6,9 @@
 #
 # [*command*]
 #   the command to run.
+#   if the command is not fully qualified, the plugin_path will be prepended
+# [*plugin_path*]
+#   specify an alternate path to the plugin dir
 #
 # === Examples
 #
@@ -23,10 +26,17 @@
 #
 define nrpe::command (
   $command,
+  $plugin_path = $nrpe::params::plugin_path,
 ) {
   include nrpe::params
 
+  if ($command !~ /^\//) {
+    $real_command = "${plugin_path}/${command}"
+  } else {
+    $real_command = $command
+  }
+
   file { "${nrpe::params::conf_d_dir}/${name}.cfg":
-    content => "command[${name}]=${command}",
+    content => "command[${name}]=${real_command}",
   }
 }
